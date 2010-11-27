edcb83878f0d1e1b0943bd812bf82375c7392bfa
Eduardo Habkost <ehabkost@raisama.net>
2010-11-26 23:36:05 -0200

    mais itens na trilha sonora
    - informação sobre neutros
    - passos para a próxima referência
    
    Signed-off-by: Eduardo Habkost <ehabkost@raisama.net>

--- enduroape/sound.py
+++ enduroape/sound.py
@@ -21,7 +21,7 @@ class SoundGenerator:
     def silence(self, samples):
         while samples > 0:
             n = min(samples, MAX_SAMPLES)
-            self.gen('\0'*BYTES*n, n)
+            self.write('\0'*BYTES*n, n)
             samples -= n
 
     def sox_cmd(self, args, samples=-1, wait=True):
@@ -37,7 +37,7 @@ class SoundGenerator:
             assert (bytes % BYTES) == 0 # whole number of samples
             n = bytes/BYTES
 
-            self.gen(data, n)
+            self.write(data, n)
             s -= n
         if wait:
             proc.wait()
@@ -62,12 +62,29 @@ class SoundGenerator:
             self.digit(digit)
 
 
+class MemoryTrack(SoundGenerator):
+    def __init__(self):
+        self.data = []
+        self.samples = 0
+
+    @property
+    def seconds(self):
+        return self.samples/float(RATE)
+
+    def write(self, b, samples):
+        self.data.append( (b, samples) )
+        self.samples += samples
+
+    def write_to(self, other):
+        for b, samples in self.data:
+            other.write(b, samples)
+
 class SoundWriter(SoundGenerator):
     def __init__(self, raw_out):
         self.raw_out = raw_out
         self.samples = 0
 
-    def gen(self, b, samples):
+    def write(self, b, samples):
         """Write raw samples
 
         The 'samples' parameter is just a sanity check to make sure
@@ -80,30 +97,87 @@ class SoundWriter(SoundGenerator):
     def cur_time(self):
         return self.samples/float(RATE)
 
+    def time_to(self, t):
+        return t-self.cur_time()
+
     def samples_to(self, t):
         """Number of samples needed to go to time 't'"""
-        cur = self.cur_time()
-        if cur > t:
-            raise Exception("too late. we are already past %r (we're at %r)" % (t, cur))
-        return int((t-cur)*RATE)
+        return int(self.time_to(t)*RATE)
 
+    def silence_to(self, t):
+        samples = self.samples_to(t)
+        if samples < 0:
+            return False
+        self.silence(samples)
+        return True
+
+    def mem_tracks(self, *tracks):
+        for t in tracks:
+            t.write_to(self)
+
+def word_track(word):
+    t = MemoryTrack()
+    t.word(word)
+    return t
+
+def number_track(n):
+    t = MemoryTrack()
+    t.number(n)
+    return t
+
+def seconds(*tracks):
+    return sum(t.seconds for t in tracks)
 
 def generate_soundtrack(filename, items):
     proc = subprocess.Popen(['sox']+SOX_ARGS+['-','-t','wav',filename], stdin=subprocess.PIPE)
     f = proc.stdin
     w = SoundWriter(f)
+
+    distnext = word_track('distanciaparaproxima')
+    distancia = word_track('distancia')
+    metros = word_track('metros')
+    passos = word_track('passos')
+
     for state,i in items:
-        if i.is_a('Referencia') or i.is_a('Neutro'):
+        if i.is_a('Referencia'):
             dbg("abs_time: %r", i.abs_time)
             dbg("we're at: %r", w.cur_time())
-            if i.abs_time > w.cur_time():
-                w.silence(w.samples_to(i.abs_time))
+
+            npassos = number_track(int(i.rel_passos))
+            nmetros = number_track(i.rel_dist)
+
+            remaining = w.time_to(i.abs_time)
+            if False: ### remaining > seconds(distnext, npassos, passos, nmetros, metros):
+                w.mem_tracks(distnext, npassos, passos, nmetros, metros)
+            elif remaining > seconds(distnext, npassos, passos):
+                w.mem_tracks(distnext, npassos, passos)
+            elif remaining > seconds(distancia, npassos, passos):
+                w.mem_tracks(distancia, npassos, passos)
             else:
-                info("Pouco tempo para o som. Ref: %r. Tempo ref: %r. Trmepo som: %r", i.ref_id, i.abs_time, w.cur_time())
+                info("sem tempo para aviso de passos. ref_id: %r", i.ref_id)
+
+            desc = MemoryTrack()
+            desc.wav_file('sounds/instrucoes/2010-11/ref%s.wav' % (i.ref_id))
+            desc.word('referencia')
+            desc.number(int(i.ref_id))
+
+            before_desc = i.abs_time - desc.seconds
+            if not w.silence_to(before_desc):
+                info("Pouco tempo para o som. atraso de %r segundos Ref: %r. Tempo ref: %r. tempo som: %r", w.cur_time()-before_desc, i.ref_id, i.abs_time, w.cur_time())
+
+            w.mem_tracks(desc)
             dbg("we're now at: %r", w.cur_time())
-            w.word('referencia')
-            w.number(int(i.ref_id))
-            w.wav_file('sounds/instrucoes/2010-11/ref%s.wav' % (i.ref_id))
+        elif i.is_a('Neutro'):
+            tempo = i.abs_time-state.prev_abs_time
+
+            w.word('neutrode')
+            w.number(tempo)
+            w.word('segundos')
+
+            w.silence_to(i.abs_time-10)
+            w.word('10-segundos-neutro')
+            w.silence_to(i.abs_time)
+            w.word('neutro-acabou')
 
     f.close()
     proc.wait()
